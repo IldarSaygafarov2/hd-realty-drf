86236876054bd0bac495ad0d5e4329c3fa11d6db
IldarSaygafarov2 <sayildar17@gmail.com>
2026-05-08 13:47:11 +0500

    added fixes

--- core/apps/consultations/views.py
+++ core/apps/consultations/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
 
 from . import models, serializers
 from rest_framework import generics
 
 
+@extend_schema(tags=["Consulations"])
 class ConsulationRequestCreateView(generics.CreateAPIView):
     queryset = models.ConsultationRequest
     # serializer_class = serializers.ConsultationRequestCreationSerializer
